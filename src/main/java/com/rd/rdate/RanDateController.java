2bc41d9654e309117d1548b36805efe0fe7783bc
Pavlent1yy <phuharev@gmail.com>
2023-12-28 20:04:41 +0300

    Randate 1.1.0

--- src/main/java/com/rd/rdate/RanDateController.java
+++ src/main/java/com/rd/rdate/RanDateController.java
@@ -15,6 +15,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
@@ -23,32 +25,64 @@ import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 /*TO-DO--------------------------------- 
-    Прикрутить еще пару-тройку режимов
-    Заполнить отстаток radioText'ов 
-    Перекраска кнопки Check /
-    Баг с логикой кнопки Check
-
+     --- Прикрутить еще пару-тройку режимов
+     --- Заполнить отстаток radioText'ов 
+     --- Перекраска кнопки Check /
+     --- Баг с перемещением файла из изночальной деректории
+     --- Баг с одним и тем же пунктом в radioLabel'e
 ----------------------------------------*/
 public class RanDateController {
 
     @FXML
-    private Button checkAnswerButton, openFileButton;
+    private Button checkAnswerButton, openFileButton, closeButton, minButton;
 
     @FXML
-    private Text isCorrectText, dateLabel, radioText1, radioText2, radioText3, radioText4;
+    private Text isCorrectText, dateLabel, correctAnswerCountText, radioText1, radioText2, radioText3, radioText4;
 
     @FXML
     private RadioButton radioButton1, radioButton2, radioButton3, radioButton4;
 
-    File file;
-    Stage stage;
-    RadioButton rightButton;
-    Map<String, String> dateMap = new HashMap<String, String>();
-    List<String> eventArray = new ArrayList<>();
-    List<String> dateArray = new ArrayList<>();
+    @FXML
+    private Pane topPane;
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private File file;
+    private Stage stage;
+    private int correctAnswerCount = 0;
+    private RadioButton rightButton;
+    private Map<String, String> dateMap = new HashMap<String, String>();
+    private List<String> eventArray = new ArrayList<>();
+    private List<String> dateArray = new ArrayList<>();
 
     @FXML
-    void openFile(ActionEvent event) {
+    protected void closeWindow(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    protected void minimizeWndow(ActionEvent event) {
+        Stage stage = (Stage) minButton.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    protected void handleClickAction(MouseEvent event) {
+        Stage stage = (Stage) topPane.getScene().getWindow();
+        xOffset = stage.getX() - event.getSceneX();
+        yOffset = stage.getY() - event.getSceneY();
+    }
+
+    @FXML
+    protected void handleMovementAction(MouseEvent event) {
+        Stage stage = (Stage) topPane.getScene().getWindow();
+        stage.setX(event.getSceneX() + xOffset);
+        stage.setY(event.getSceneY() + yOffset);
+
+    }
+
+    @FXML
+    protected void openFile(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select a file with dates");
         fileChooser.getExtensionFilters().add(new ExtensionFilter("*.txt files",
@@ -59,7 +93,7 @@ public class RanDateController {
         readFile();
     }
 
-    void readFile() {
+    protected void readFile() {
         if (file != null) {
             try {
                 BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -73,48 +107,63 @@ public class RanDateController {
                 dateArray.addAll(dateMap.keySet());
                 reader.close();
             } catch (IOException e) {
-                System.out.println("readFile error");
+                System.out.println(e.getClass().getSimpleName());
             }
-            raiseQuestion();
+            makeTheQuestion();
         }
     }
 
     @FXML
-    void checkAnswer(ActionEvent event) throws InterruptedException {
+    protected void checkAnswer(ActionEvent event) throws InterruptedException {
         if (file != null) {
             if (rightButton.isSelected()) {
+                Thread.sleep(250);
                 isCorrectText.setFill(Color.GREEN);
+                correctAnswerCount += 1;
+                correctAnswerCountText.setText(correctAnswerCount + "\\20");
             } else {
+                Thread.sleep(250);
                 isCorrectText.setFill(Color.RED);
+                correctAnswerCount -= 1;
+                correctAnswerCountText.setText(correctAnswerCount + "\\20");
             }
             readFile();
-            rightButton.setSelected(true);
+            rightButton.requestFocus();
         }
 
     }
 
-    void raiseQuestion() {
-        Random rand = new Random();
-        String date = dateArray.get(rand.nextInt(dateArray.size()));
-        String event = dateMap.get(date);
-        List<Text> radioTextArray = new ArrayList<Text>();
-        List<RadioButton> radioButtonsArray = new ArrayList<RadioButton>();
-        Collections.addAll(radioTextArray, radioText1, radioText2, radioText3, radioText4);
-        Collections.addAll(radioButtonsArray, radioButton1, radioButton2, radioButton3, radioButton4);
+    protected void makeTheQuestion() {
+        try {
+            Random rand = new Random();
+            String date = dateArray.get(rand.nextInt(dateArray.size()));
+            String event = dateMap.get(date);
+            List<Text> radioTextArray = new ArrayList<Text>();
+            List<RadioButton> radioButtonsArray = new ArrayList<RadioButton>();
+            Collections.addAll(radioTextArray, radioText1, radioText2, radioText3, radioText4);
+            Collections.addAll(radioButtonsArray, radioButton1, radioButton2, radioButton3, radioButton4);
 
-        int randomElement = rand.nextInt(radioButtonsArray.size());
-        rightButton = radioButtonsArray.get(randomElement);
-        Text rightText = radioTextArray.get(randomElement);
-        radioTextArray.remove(rightText);
-        for (Text t : radioTextArray) {
-            t.setText(eventArray.get(rand.nextInt(eventArray.size())));
+            int randomElement = rand.nextInt(radioButtonsArray.size());
+            rightButton = radioButtonsArray.get(randomElement);
+            Text rightText = radioTextArray.get(randomElement);
+            radioTextArray.remove(rightText);
+            for (Text t : radioTextArray) {
+                t.setText(eventArray.get(rand.nextInt(eventArray.size())));
+            }
+
+            //
+            dateLabel.setText("Date: " + date);
+            rightText.setText(event);
+            dateArray.clear();
+            eventArray.clear();
+
+        } catch (IllegalArgumentException e) {
+            errorPopup(e);
         }
+    }
 
-        //
-        dateLabel.setText("Date: " + date);
-        rightText.setText(event);
-        dateArray.clear();
-        eventArray.clear();
+    protected void errorPopup(Exception e) { // TODO Error Popup
+        System.out.println(e.getClass().getSimpleName());
     }
 
 }
--- src/main/java/com/rd/rdate/RandateApp.java
+++ src/main/java/com/rd/rdate/RandateApp.java
@@ -3,7 +3,9 @@ package com.rd.rdate;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,7 +13,10 @@ public class RandateApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RanDateController.class.getResource("RUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 890, 520);
+        Scene scene = new Scene(fxmlLoader.load(), 890, 585);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Randate");
         stage.setScene(scene);
         stage.setResizable(false);
